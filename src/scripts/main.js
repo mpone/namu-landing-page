811f59e1bc648d12a65331b3fb8746f64f3e2634
mpone <m.parphenjuk@gmail.com>
2020-09-12 02:28:45 +0300

    Version 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const subscriptionForm = document.querySelector('.subscription__form');
+
+subscriptionForm.addEventListener('submit', event => {
+  event.preventDefault();
+  subscriptionForm.reset();
+});
